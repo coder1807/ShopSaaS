c44337c52d5df2a35d34d932eac0995224f946c0
Bui Hoang Viet <bhviet.910lhphong.cr@gmail.com>
2025-06-21 11:30:50 +0700

    bugfix(ioredis): simplify Redis initialization and add nx reset cache cli

--- packages/libs/redis/index.ts
+++ packages/libs/redis/index.ts
@@ -1,9 +1,6 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD,
-});
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const redis = new Redis(process.env.REDIS_DATABASE_URI!);
 
 export default redis;
