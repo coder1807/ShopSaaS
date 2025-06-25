99cf8f4f45abaf93fb54ea5ae4ef41b4d9d259fc
Bui Hoang Viet <bhviet.910lhphong.cr@gmail.com>
2025-06-25 23:59:08 +0700

    feat(header): create header component and update layout to include it

---
+++ apps/user-ui/src/app/shared/widgets/header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <div className="w-full bg-white">
+        <div className="w-[80%] py-5 m-auto flex items-center justify-between">
+          <div>
+            <Link href={}></Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
