156291a36df26596a7d4f462bb897e9fb48b9bb3
Bui Hoang Viet <bhviet.910lhphong.cr@gmail.com>
2025-06-26 23:52:34 +0700

    feat(header): add HeaderBottom component and enhance header layout with icons

--- apps/user-ui/src/app/layout.tsx
+++ apps/user-ui/src/app/layout.tsx
@@ -1,5 +1,18 @@
 import './global.css';
 import Header from './shared/widgets/header';
+import { Poppins, Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-roboto',
+});
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
+});
 
 export const metadata = {
   title: 'Welcome to ShopSaaS',
@@ -14,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${poppins.variable} ${roboto.variable}`}>
         <Header />
         {children}
       </body>
