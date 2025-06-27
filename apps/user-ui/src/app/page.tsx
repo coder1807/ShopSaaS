a8c3085f2223004a3b3eb4f4bffdc2f2122cb350
Bui Hoang Viet <bhviet.910lhphong.cr@gmail.com>
2025-06-27 11:18:20 +0700

    feat(header): complete implement navigation links and enhance header fixed while scroll screen

--- apps/user-ui/src/app/page.tsx
+++ apps/user-ui/src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const Page = () => {
-  return <div>Page</div>;
+  return <div className=""></div>;
 };
 export default Page;
--- apps/user-ui/src/shared/widgets/header-bottom.tsx
+++ apps/user-ui/src/shared/widgets/header-bottom.tsx
@@ -1,6 +1,14 @@
 'use client';
-import { AlignLeft, ChevronDown } from 'lucide-react';
+import {
+  AlignLeft,
+  ChevronDown,
+  HeartIcon,
+  ShoppingCart,
+  UserIcon,
+} from 'lucide-react';
 import React, { useEffect, useState } from 'react';
+import { navItems } from '../../configs/constants';
+import Link from 'next/link';
 
 const HeaderBottom = () => {
   const [show, setShow] = useState(false);
@@ -53,7 +61,49 @@ const HeaderBottom = () => {
         )}
 
         {/* Navigation Link */}
-        <div className="flex items-center"></div>
+        <div className="flex items-center">
+          {navItems.map((i: NavItemsTypes, index: number) => (
+            <Link
+              key={index}
+              href={`${i.href}`}
+              className="px-5 font-medium text-lg"
+            >
+              {i.title}
+            </Link>
+          ))}
+        </div>
+        <div>
+          {isSticky && (
+            <div className="flex items-center gap-8 pb-2">
+              <div className="flex items-center gap-2">
+                <Link
+                  href={'/login'}
+                  className="border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a]"
+                >
+                  <UserIcon />
+                </Link>
+                <Link href={'/login'}>
+                  <span className="block font-medium">Hello, </span>
+                  <span className="font-semibold">Sign In</span>
+                </Link>
+              </div>
+              <div className="flex items-center gap-5">
+                <Link href={'/wishlist'} className="relative">
+                  <HeartIcon />
+                  <div className="w-6 h-6 border-2 border-white bg-red-500 rounded-full flex items-center justify-center absolute top-[-10px] right-[-10px]">
+                    <span className="text-white text-sm font-medium">0</span>
+                  </div>
+                </Link>
+                <Link href={'/cart'} className="relative">
+                  <ShoppingCart />
+                  <div className="w-6 h-6 border-2 border-white bg-red-500 rounded-full flex items-center justify-center absolute top-[-10px] right-[-10px]">
+                    <span className="text-white text-sm font-medium">0</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
