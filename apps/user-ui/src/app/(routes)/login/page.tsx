2adb0859d71eecc395079dcf2e154e5a507d15e1
Bui Hoang Viet <bhviet.910lhphong.cr@gmail.com>
2025-07-02 23:39:16 +0700

    feat(login): integrate GoogleButton component and add signup link to login page

--- apps/user-ui/src/app/(routes)/login/page.tsx
+++ apps/user-ui/src/app/(routes)/login/page.tsx
@@ -1,4 +1,7 @@
 'use client';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import GoogleButton from 'apps/user-ui/src/shared/components/google-button';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -20,7 +23,34 @@ const Login = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  return <div></div>;
+  const onSubmit = (data: FormData) => {};
+
+  return (
+    <div className="w-full py-10 min-h-[85vh] bg-[#f1f1f1]">
+      <h1 className="text-4xl font-Poppins font-semibold text-black text-center">
+        Login
+      </h1>
+      <p className="text-center text-lg font-medium py-3 text-[#00000099]">
+        Home . Login
+      </p>
+
+      <div className="w-full flex justify-center">
+        <div className="md:w-[480px] p-8 bg-white shadow rounded-lg">
+          <h3 className="text-3xl font-semibold text-center mb-2">
+            Login to ShopSaaS
+          </h3>
+          <p className="text-center text-gray-500 mb-4">
+            {"Don't have an account?"}
+            <Link href={'/signup'} className="text-blue-500 ml-1">
+              Sign up
+            </Link>
+          </p>
+
+          <Google
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
